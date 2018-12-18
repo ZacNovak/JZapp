4055f70eb892b612b4c7907bc52a89a060195680
ZacNovak <zac.w.novak@gmail.com>
2018-12-18 13:07:44 -0700

    fixed popup

--- front-end/src/Components/ClientsList.js
+++ front-end/src/Components/ClientsList.js
@@ -12,6 +12,8 @@ class ClientComp extends Component {
         
         this.updateclients = this.props.updateclients;
     }
+
+
     
     render() {
         if (this.props.clientsToShow) {
@@ -22,6 +24,8 @@ class ClientComp extends Component {
                     rmFunc={this.props.rmFunc}
                     updateclients={this.props.updateclients}
                     clientId={this.props.clientId}
+                    openForm={this.openForm}
+                    closeForm={this.closeForm}
                     />
                     <AddNewClient updateclients={this.props.updateclients}/>
                     
@@ -55,6 +59,8 @@ function ListOfClients(props) {
             rmFunc={props.rmFunc}
             updateclients={props.updateclients}
             clientId={props.clientId}
+            openForm={props.openForm}
+            closeForm={props.closeForm}
             />,            
         );
         return (
@@ -65,11 +71,56 @@ function ListOfClients(props) {
     }
 }
 
-function SingleClient(props) {
+class SingleClient extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: " ",
+        };
+        this.updateClients = this.props.updateclients;
+        this.id = this.props.id;
+    }
     
+    on = (event) => {
+        document.getElementById("overlay").style.display = "block";
+        event.preventDefault();
+        
+
+    }
+      
+    off = (event) => {
+        document.getElementById("overlay").style.display = "none";
+    }
+
+    handleChange = (event) => {
+        this.setState({name: event.target.value});
+    }
+    
+    handleSubmit = (event) => {
+        console.log('A name was submitted: ' + this.state.name +'with an id of ' + this.id);
+        event.preventDefault();
+
+        let data = {
+            id: this.id,
+            name:this.state.name
+        }
+ 
+        fetch('http://localhost:5000/updateClient', {
+                method: 'put',
+                headers: {'Content-type':'application/json'},
+                body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(this.updateClients)
+            .then(this.off)
+    }
+
+    
+
+    render(){
     let classType = "";
-    let id = props.id;
-    let clientId = props.clientId;
+    let id = this.props.id;
+    let clientId = this.props.clientId;
 
     if(parseInt(id) === parseInt(clientId)){    
         classType = "clientCardSelected"
@@ -77,19 +128,40 @@ function SingleClient(props) {
         classType = "clientCard"
     }
 
+
+
     return(
         
         <div>
-            <div >
-                <div className={classType} id={props.id} onClick={props.onClient}>
-                    <h2 className = "clientName">{props.name}</h2>
-                    <button id={props.id} onClick={props.rmFunc}>X</button>
-                    <UpdateClient id={props.id} updateClients={props.updateclients}/>
+            <div>
+                <div className={classType} id={this.props.id} onClick={this.props.onClient}>
+                    <h2 className = "clientName">{this.props.name}</h2>
+                    <button id={this.props.id} onClick={this.props.rmFunc}>X</button>
+                    <button onClick={this.on}>Edit</button>
+                    <div id="overlay" >
+                        <div id="modal">
+                            <h1>Update Client</h1>
+                            <br/>
+                            <br/>
+                            <div className="form">
+                                <form onSubmit={this.handleSubmit}>
+                                    <label>
+                                        Name:
+                                        <input type="text" value={this.state.name} onChange={this.handleChange}/>
+                                        </label>
+                                    <input className="button" type="submit" value="Submit" />
+                                </form>
+                                <button onClick={this.off}>Exit</button>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
 
     );
+}
 }
 
 
