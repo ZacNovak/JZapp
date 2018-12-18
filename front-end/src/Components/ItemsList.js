714000815b44b3a76281cc3c29027f4095176fec
Julia Pulwicki <jpulwicki@gmail.com>
2018-12-18 14:08:17 -0700

    create and delete items

--- front-end/src/Components/ItemsList.js
+++ front-end/src/Components/ItemsList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import AddNewClient from './AddNewItem';
+import AddNewItem from './AddNewItem';
 
 class ItemsList extends Component {
 
@@ -7,9 +7,12 @@ class ItemsList extends Component {
         if (this.props.itemsToShow) {
             return (
                 <div>
-                    <ListOfItems itemsList={this.props.itemsToShow}/>
-                    <AddNewClient 
-                    updateItemsList={this.props.updateItemList}
+                    <ListOfItems 
+                    itemsList={this.props.itemsToShow}
+                    removeItem={this.props.removeItem}
+                    />
+                    <AddNewItem 
+                    updateItemList={this.props.updateItemList}
                     invoiceId={this.props.invoiceId}
                     />
                 </div>
@@ -35,6 +38,7 @@ function ListOfItems(props) {
             quantity={itemsArr[i].quantity}
             name={itemsArr[i].name}
             price={itemsArr[i].price}
+            removeItem={props.removeItem}
             />
         );
         return (
@@ -51,6 +55,7 @@ function SingleItem(props) {
             <div className='itemCard' id={props.id}>
                 <h2>{props.name}</h2> <br></br>
                 <h2>Quantity: {props.quantity}, Price: {props.price}</h2> 
+                <button id={props.id} onClick={props.removeItem}>x</button>
             </div>
         </div>
 
